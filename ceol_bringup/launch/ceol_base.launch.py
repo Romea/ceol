154c0d773f0b224d3f551a39f55a17c943ac9c1e
Cyrille Pierre <cyrille.pierre@inrae.fr>
2025-01-27 15:36:35 +0100

    use angular_speed_topic when calling controller

--- ceol_bringup/launch/ceol_base.launch.py
+++ ceol_bringup/launch/ceol_base.launch.py
@@ -32,17 +32,15 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def launch_setup(context, *args, **kwargs):
-
     mode = LaunchConfiguration("mode").perform(context)
+    base_name = LaunchConfiguration("base_name").perform(context)
+    robot_namespace = LaunchConfiguration("robot_namespace").perform(context)
 
     if "replay" in mode:
         return []
 
     if mode == "simulation":
         mode += "_gazebo_classic"
-
-    base_name = LaunchConfiguration("base_name").perform(context)
-    robot_namespace = LaunchConfiguration("robot_namespace").perform(context)
 
     if robot_namespace:
         controller_manager_name = "/" + robot_namespace + "/base/controller_manager"
@@ -61,8 +59,7 @@ def launch_setup(context, *args, **kwargs):
 
     if "live" in mode:
         base_controller_yaml_file = (
-            get_package_share_directory("ceol_bringup")
-            + "/config/mobile_base_controller_live.yaml"
+            get_package_share_directory("ceol_bringup") + "/config/mobile_base_controller_live.yaml"
         )
     else:
         base_controller_yaml_file = (
@@ -70,7 +67,7 @@ def launch_setup(context, *args, **kwargs):
             + "/config/mobile_base_controller_simulation.yaml"
         )
 
-    base_ros2_control_description_file = "/tmp/"+robot_prefix+base_name+"_ros2_control.urdf"
+    base_ros2_control_description_file = "/tmp/" + robot_prefix + base_name + "_ros2_control.urdf"
     with open(base_ros2_control_description_file, "r") as f:
         base_ros2_control_description = f.read()
 
@@ -80,7 +77,8 @@ def launch_setup(context, *args, **kwargs):
         executable="ros2_control_node",
         parameters=[
             {"robot_description": base_ros2_control_description},
-            controller_manager_yaml_file],
+            controller_manager_yaml_file,
+        ],
     )
 
     controller = IncludeLaunchDescription(
@@ -101,6 +99,7 @@ def launch_setup(context, *args, **kwargs):
             "controller_manager_name": controller_manager_name,
             "base_description_yaml_filename": base_description_yaml_file,
             "base_controller_yaml_filename": base_controller_yaml_file,
+            "angular_speed_input_topic": f"/{robot_namespace}/imu/data",
         }.items(),
         condition=LaunchConfigurationNotEquals("mode", "replay"),
     )
@@ -130,19 +129,10 @@ def launch_setup(context, *args, **kwargs):
 
 
 def generate_launch_description():
+    declared_arguments = [
+        DeclareLaunchArgument("mode", default_value="simulation"),
+        DeclareLaunchArgument("robot_namespace", default_value="ceol"),
+        DeclareLaunchArgument("base_name", default_value="base"),
+    ]
 
-    declared_arguments = []
-
-    declared_arguments.append(DeclareLaunchArgument("mode", default_value="simulation"))
-
-    declared_arguments.append(
-        DeclareLaunchArgument("robot_namespace", default_value="ceol")
-    )
-
-    declared_arguments.append(
-        DeclareLaunchArgument("base_name", default_value="base")
-    )
-
-    return LaunchDescription(
-        declared_arguments + [OpaqueFunction(function=launch_setup)]
-    )
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
